50dc04efc2455c8f0061f610b2651c070ba46ac4
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-06-18 16:52:51 -0400

    refining stylelint rules, adding base structure for sass files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,15 @@ module.exports = {
   'rules': {
     'rule-empty-line-before': 'never-multi-line',
     'no-empty-source': null,
-  }
+    'at-rule-no-unknown': [true, {
+      'ignoreAtRules': ['function', 'if', 'each', 'include', 'mixin', 'error', 'return', 'else'],
+    }],
+    'color-hex-case': 'lower',
+    'color-hex-length': 'long',
+    'color-named': 'never',
+    'declaration-no-important': true,
+    'string-quotes': 'single',
+    'unit-case': 'lower',
+    'selector-pseudo-element-colon-notation': 'single',
+  },
 };
